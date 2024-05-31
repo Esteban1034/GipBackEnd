f9d6f4d7df97f73809f0964e99805ad24749e537
Sleider Jair Sevillano Ramos <ssevillano@itssolutions.co>
2024-05-31 09:36:35 -0500

    Creacion endpoint todos los metodos probados update,get,delete,put findId

---
+++ back-end-gpi/src/main/java/com/backendgip/service/EstimacionesUfsService.java
@@ -0,0 +1,16 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package com.backendgip.service;
+import com.backendgip.model.EstimacionUfs;
+import java.util.List;
+
+public interface EstimacionesUfsService {
+	List<EstimacionUfs> getEstimaciones();  
+
+    
+    EstimacionUfs saveEstimaciones(EstimacionUfs estimacionesUfs);
+    void deleteEstimaciones(EstimacionUfs estimacionesUfs);
+}
